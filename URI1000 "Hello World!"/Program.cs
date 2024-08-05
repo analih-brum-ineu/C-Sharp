82d38f8438e2cec934e82d025759e268c5279bfa
Analih Brum Ineu <168863819+analih-brum-ineu@users.noreply.github.com>
2024-08-05 09:34:19 -0300

    Create Program.cs

---
+++ URI1000 "Hello World!"/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace URI1000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
